c9588fb6cb2d68ebb491a614720537a95f975319
ancplua <anfh22@outlook.com>
2026-06-04 17:55:53 +0200

    Preserve GraphQL async exception timing

--- src/Qyl.AutoInstrumentation/QylInterceptedGraphQl.cs
+++ src/Qyl.AutoInstrumentation/QylInterceptedGraphQl.cs
@@ -29,6 +29,36 @@ public static class QylInterceptedGraphQl
     {
     }
 
+    public static Task<T> ObserveAsync<T>(Task<T>? task, Activity? activity)
+    {
+        if (activity is null || task is null)
+        {
+            activity?.Dispose();
+            return task!;
+        }
+
+        return ObserveSlowAsync(task, activity);
+    }
+
+    private static async Task<T> ObserveSlowAsync<T>(Task<T> task, Activity activity)
+    {
+        try
+        {
+            var result = await task.ConfigureAwait(false);
+            RecordSuccess(activity);
+            return result;
+        }
+        catch (Exception exception)
+        {
+            RecordException(activity, exception);
+            throw;
+        }
+        finally
+        {
+            activity.Dispose();
+        }
+    }
+
     public static void RecordException(Activity? activity, Exception exception)
     {
         activity?.SetTag(QylSemanticAttributes.ErrorType, exception.GetType().Name);
