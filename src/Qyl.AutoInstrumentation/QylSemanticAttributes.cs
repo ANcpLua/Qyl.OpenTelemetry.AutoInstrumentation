57ad3ffb23104ba95b83b0e17106839192e285a1
ancplua <anfh22@outlook.com>
2026-06-04 17:10:24 +0200

    Use generated semconv attribute constants

--- src/Qyl.AutoInstrumentation/QylSemanticAttributes.cs
+++ src/Qyl.AutoInstrumentation/QylSemanticAttributes.cs
@@ -1,35 +1,54 @@
 namespace Qyl.AutoInstrumentation;
 
+using DbAttributes = Qyl.OpenTelemetry.SemanticConventions.Attributes.Db.DbAttributes;
+using ErrorAttributes = Qyl.OpenTelemetry.SemanticConventions.Attributes.Error.ErrorAttributes;
+using GraphqlAttributes = Qyl.OpenTelemetry.SemanticConventions.Incubating.Attributes.Graphql.GraphqlAttributes;
+using HttpAttributes = Qyl.OpenTelemetry.SemanticConventions.Attributes.Http.HttpAttributes;
+using MessagingAttributes = Qyl.OpenTelemetry.SemanticConventions.Incubating.Attributes.Messaging.MessagingAttributes;
+using RpcAttributes = Qyl.OpenTelemetry.SemanticConventions.Incubating.Attributes.Rpc.RpcAttributes;
+using ServerAttributes = Qyl.OpenTelemetry.SemanticConventions.Attributes.Server.ServerAttributes;
+using UrlAttributes = Qyl.OpenTelemetry.SemanticConventions.Attributes.Url.UrlAttributes;
+
 internal static class QylSemanticAttributes
 {
     public const string QylInstrumentationDomain = "qyl.instrumentation.domain";
-    public const string HttpRequestMethod = "http.request.method";
-    public const string HttpResponseStatusCode = "http.response.status_code";
-    public const string HttpRequestHeaderPrefix = "http.request.header.";
-    public const string HttpResponseHeaderPrefix = "http.response.header.";
-    public const string HttpRoute = "http.route";
-    public const string UrlPath = "url.path";
-    public const string UrlQuery = "url.query";
-    public const string DbSystemName = "db.system.name";
-    public const string DbNamespace = "db.namespace";
-    public const string DbOperationName = "db.operation.name";
-    public const string DbQuerySummary = "db.query.summary";
-    public const string DbQueryText = "db.query.text";
-    public const string RpcSystem = "rpc.system";
-    public const string RpcService = "rpc.service";
-    public const string RpcMethod = "rpc.method";
-    public const string RpcGrpcStatusCode = "rpc.grpc.status_code";
+
+    public const string HttpRequestMethod = HttpAttributes.RequestMethod;
+    public const string HttpResponseStatusCode = HttpAttributes.ResponseStatusCode;
+    public const string HttpRequestHeaderPrefix = HttpAttributes.RequestHeader + ".";
+    public const string HttpResponseHeaderPrefix = HttpAttributes.ResponseHeader + ".";
+    public const string HttpRoute = HttpAttributes.Route;
+
+    public const string UrlPath = UrlAttributes.Path;
+    public const string UrlQuery = UrlAttributes.Query;
+    public const string UrlFull = UrlAttributes.Full;
+
+    public const string DbSystemName = DbAttributes.SystemName;
+    public const string DbNamespace = DbAttributes.Namespace;
+    public const string DbOperationName = DbAttributes.OperationName;
+    public const string DbQuerySummary = DbAttributes.QuerySummary;
+    public const string DbQueryText = DbAttributes.QueryText;
+
+#pragma warning disable CS0618 // Qyl still mirrors the current OTEL .NET auto gRPC status attribute contract.
+    public const string RpcSystem = RpcAttributes.System;
+    public const string RpcService = RpcAttributes.Service;
+    public const string RpcMethod = RpcAttributes.Method;
+    public const string RpcGrpcStatusCode = RpcAttributes.GrpcStatusCode;
+#pragma warning restore CS0618
     public const string GrpcRequestMetadataPrefix = "grpc.request.metadata.";
     public const string GrpcResponseMetadataPrefix = "grpc.response.metadata.";
-    public const string MessagingSystem = "messaging.system";
-    public const string MessagingOperationName = "messaging.operation.name";
-    public const string MessagingDestinationName = "messaging.destination.name";
+
+    public const string MessagingSystem = MessagingAttributes.System;
+    public const string MessagingOperationName = MessagingAttributes.OperationName;
+    public const string MessagingDestinationName = MessagingAttributes.DestinationName;
+
     public const string LogSeverity = "log.severity";
     public const string LogEventName = "log.event.name";
-    public const string GraphQlOperationName = "graphql.operation.name";
-    public const string GraphQlDocument = "graphql.document";
-    public const string ServerAddress = "server.address";
-    public const string ServerPort = "server.port";
-    public const string UrlFull = "url.full";
-    public const string ErrorType = "error.type";
+
+    public const string GraphQlOperationName = GraphqlAttributes.OperationName;
+    public const string GraphQlDocument = GraphqlAttributes.Document;
+
+    public const string ServerAddress = ServerAttributes.Address;
+    public const string ServerPort = ServerAttributes.Port;
+    public const string ErrorType = ErrorAttributes.Type;
 }
