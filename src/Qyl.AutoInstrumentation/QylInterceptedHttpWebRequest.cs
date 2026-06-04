9762d8e755c679a7be3e6918269dff8cc04478d9
ancplua <anfh22@outlook.com>
2026-06-04 20:33:18 +0200

    Capture HttpWebRequest error responses

--- src/Qyl.AutoInstrumentation/QylInterceptedHttpWebRequest.cs
+++ src/Qyl.AutoInstrumentation/QylInterceptedHttpWebRequest.cs
@@ -46,22 +46,37 @@ public static class QylInterceptedHttpWebRequest
         int? statusCode = null;
         var response = result as HttpWebResponse;
         if (response is not null)
-            statusCode = (int)response.StatusCode;
-
-        if (activity is not null && response is not null)
-        {
-            activity.SetTag(QylSemanticAttributes.HttpResponseStatusCode, statusCode);
-            SetConfiguredHeaders(activity, QylSemanticAttributes.HttpResponseHeaderPrefix, QylAutoInstrumentationOptions.Current.HttpClientCapturedResponseHeaders, response.Headers);
-        }
+            statusCode = RecordResponse(activity, response, markErrorForStatus: true);
 
         RecordDuration(startTimeUtc, method, statusCode);
     }
 
     public static void RecordException(Activity? activity, DateTime startTimeUtc, string? method, Exception exception)
     {
+        int? statusCode = null;
+        if (exception is WebException { Response: HttpWebResponse response })
+            statusCode = RecordResponse(activity, response, markErrorForStatus: false);
+
         activity?.SetTag(QylSemanticAttributes.ErrorType, exception.GetType().Name);
         activity?.SetStatus(ActivityStatusCode.Error);
-        RecordDuration(startTimeUtc, method, null);
+        RecordDuration(startTimeUtc, method, statusCode);
+    }
+
+    private static int RecordResponse(Activity? activity, HttpWebResponse response, bool markErrorForStatus)
+    {
+        var statusCode = (int)response.StatusCode;
+        if (activity is not null)
+        {
+            activity.SetTag(QylSemanticAttributes.HttpResponseStatusCode, statusCode);
+            SetConfiguredHeaders(activity, QylSemanticAttributes.HttpResponseHeaderPrefix, QylAutoInstrumentationOptions.Current.HttpClientCapturedResponseHeaders, response.Headers);
+            if (markErrorForStatus && statusCode >= 400)
+            {
+                activity.SetTag(QylSemanticAttributes.ErrorType, statusCode.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                activity.SetStatus(ActivityStatusCode.Error);
+            }
+        }
+
+        return statusCode;
     }
 
     private static void RecordDuration(DateTime startTimeUtc, string? method, int? statusCode)
